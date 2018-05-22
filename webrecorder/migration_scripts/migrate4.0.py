b3ef25ef152ba6f64974f1b85fa2a8396a17461d
m4rk3r <mark.beasley@rhizome.org>
2018-05-22 02:05:54 +0000

    migration script fixes: - disable per-recording list by default - skip recording or collection if missing: id, created_at, updated_at - print errors for missing created_at, updated_at - skip non-s3 files (for now) - don't migrate if user already exists - remove empty user dir after done with user migration - set description for auto-generated list

--- webrecorder/migration_scripts/migrate4.0.py
+++ webrecorder/migration_scripts/migrate4.0.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import datetime
 
 # add parent dir to path to access webrecorder package
 wr_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,7 @@ class Migration(object):
     def __init__(self, old_redis_url, new_redis_url, dry_run=True):
         self.old_redis = StrictRedis.from_url(old_redis_url, decode_responses=True)
         self.dry_run = dry_run
+        self.per_recording_list = False
 
         print('DRY RUN', self.dry_run)
         if self.dry_run:
@@ -65,7 +67,7 @@ class Migration(object):
         for user in all_users:
             self.migrate_user(user)
 
-    def migrate_user(self, username, new_username=None, coll_name=None):
+    def migrate_user(self, username, new_username=None, coll_name=None, skip_if_exists=True):
         new_username = new_username or username
         print('Processing User: {0} -> {1}'.format(username, new_username))
 
@@ -90,13 +92,17 @@ class Migration(object):
             # add user to s:users, now a valid user!
             self.redis.sadd(UserTable.USERS_KEY, new_username)
         else:
+            if skip_if_exists:
+                print('User exists, skipping')
+                return
+
             print('Use already exists, migrating collections')
 
         user = self.cli.all_users.get_user(new_username)
 
         if not coll_name:
             # get all collection list
-            colls = self.old_redis.smembers(User.COMP_KEY.format(user=new_username))
+            colls = self.old_redis.smembers(User.COLLS_KEY.format(user=new_username))
         else:
             colls = [coll_name]
 
@@ -104,10 +110,21 @@ class Migration(object):
             print('  Processing Collection: ' + coll)
             self.migrate_collection(user, username, coll)
 
+        try:
+            target_dirname = user.get_user_temp_warc_path()
+            os.rmdir(target_dirname)
+        except Exception as e:
+            print('Error deleting user dir: ' + str(e))
+
+
     def migrate_collection(self, user, old_user, old_coll):
         # get old coll info data
         old_info_key = 'c:{user}:{coll}:info'.format(user=old_user, coll=old_coll)
         old_coll_data = self.old_redis.hgetall(old_info_key)
+
+        if not old_coll_data or not old_coll_data.get('id'):
+            print('  SKIPPING INVALID: ' + old_coll)
+            return
 
         collection = user.create_collection(old_coll,
                                             allow_dupe=False,
@@ -117,8 +134,12 @@ class Migration(object):
 
         print('  New Collection Created: ' + collection.my_id)
 
-        collection.set_prop('created_at', old_coll_data['created_at'])
-        collection.set_prop('size', old_coll_data['size'])
+        if old_coll_data.get('created_at'):
+            collection.set_prop('created_at', old_coll_data.get('created_at'))
+        else:
+            print('  OBJ ERR: created_at missing')
+
+        collection.set_prop('size', old_coll_data.get('size', '0'))
 
         collection.bookmarks_list = None
 
@@ -157,7 +178,7 @@ class Migration(object):
         # old rec info
         old_rec_data = self.old_redis.hgetall(old_rec_base_key + 'info')
 
-        if not old_rec_data:
+        if not old_rec_data or not old_rec_data.get('id'):
             print('SKIPPING INVALID: ' + rec)
             return
 
@@ -176,9 +197,16 @@ class Migration(object):
         print('    New Recording Created: ' + recording.my_id)
 
         # copy props
-        recording.set_prop('created_at', old_rec_data['created_at'])
-        recording.set_prop('updated_at', old_rec_data['updated_at'])
-        recording.set_prop('recorded_at', old_rec_data['updated_at'])
+        if 'created_at' in old_rec_data:
+            recording.set_prop('created_at', old_rec_data['created_at'])
+        else:
+            print('    OBJ ERR: created_at missing')
+
+        if 'updated_at' in old_rec_data:
+            recording.set_prop('updated_at', old_rec_data['updated_at'])
+            recording.set_prop('recorded_at', old_rec_data['updated_at'])
+        else:
+            print('    OBJ ERR: updated_at missing')
 
         pages = self.old_redis.hvals(old_rec_base_key + 'page')
         pages = [json.loads(page) for page in pages]
@@ -197,11 +225,18 @@ class Migration(object):
 
         # shared 'Bookmarks' list
         if not collection.bookmarks_list:
-            collection.bookmarks_list = collection.create_bookmark_list(dict(title='Your Bookmarks',
+            public_desc = 'List automatically created from visible pages on ' + self.datestring()
+
+            collection.bookmarks_list = collection.create_bookmark_list(dict(title='Bookmarks',
+                                                                             desc=public_desc,
                                                                              public=True))
         # per-recording list of public bookmarks
-        recording_list = collection.create_bookmark_list(dict(title=title,
-                                                              public=True))
+        if self.per_recording_list:
+            recording_list = collection.create_bookmark_list(dict(title=title,
+                                                                  public=False))
+        else:
+            recording_list = None
+
         # create bookmarks for visible pages
         for page in visible_pages:
             #page['rec'] = recording.my_id
@@ -209,7 +244,8 @@ class Migration(object):
 
             print('    BOOKMARK: ' + page.get('timestamp', '') + ' ' + page.get('url', ''))
 
-            bookmark_1 = recording_list.create_bookmark(page)
+            if recording_list:
+                bookmark_1 = recording_list.create_bookmark(page)
             bookmark_2 = collection.bookmarks_list.create_bookmark(page)
 
         return recording
@@ -230,6 +266,10 @@ class Migration(object):
         loader = BlockLoader()
 
         for n, url in warc_files.items():
+            if not url.startswith('s3://'):
+                print('FILE ERR: Skipping local file: ' + url)
+                continue
+
             local_filename = n if n != recording.INDEX_FILE_KEY else os.path.basename(url)
             target_file = os.path.join(target_dirname, local_filename)
 
@@ -258,6 +298,9 @@ class Migration(object):
         # commit from temp dir to storage
         if not self.dry_run:
             recording.commit_to_storage()
+
+    def datestring(self):
+        return datetime.datetime.utcnow().isoformat()[:19].replace('T', ' ')
 
 
 # ============================================================================
